f1124edcc1bc61e1e485330b12d24a80fafaa655
yanxu2021 <yan.xu2021@outlook.com>
2021-08-11 13:30:47 -0700

    Complete destroy

--- app/controllers/travel_controller.rb
+++ app/controllers/travel_controller.rb
@@ -10,4 +10,29 @@ class TravelController < ApplicationController
   def new
     @travel = Travel.new
   end
+
+  def create
+    @travel = Travel.create(travel_params)
+    if @travel.valid?
+      redirect_to travels_path
+    else
+      redirect_to new_travel_path
+    end
+  end
+
+  def destroy
+    @travel = Travel.find(params[:id])
+    if @travel.destroy
+      redirect_to travels_path
+    else
+      redirect_to travels_path(@travel)
+    end  
+  end
+
+  # Anything below private can only be called inside the scope of this class
+  private
+
+  def travel_params
+    params.require(:travel).permit(:title, :content)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get '/travels' => 'travel#index', as:'travels'
   get '/travels/new' => 'travel#new', as:'new'
   get '/travels/:id' =>'travel#show', as:'show'
+  post '/travels' =>'travel#create'
+  delete '/travels/:id' => 'travel#destroy', as: 'delete'
   root 'travel#index'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
